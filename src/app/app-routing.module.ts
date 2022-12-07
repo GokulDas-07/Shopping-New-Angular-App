c55ac5c11332c8548b8c53e24e4ff03e188e1607
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-08 02:20:45 +0530

    search product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { SearchProductComponent } from './search-product/search-product.component';
 import { ViewProductComponent } from './view-product/view-product.component';
 
 const routes: Routes = [
   {path:"",component:AdminLoginComponent},
   {path:"addproduct",component:AddProductComponent},
-  {path:"viewproduct",component:ViewProductComponent}
+  {path:"viewproduct",component:ViewProductComponent},
+  {path:"searchproduct",component:SearchProductComponent}
 ];
 
 @NgModule({
